c8dd6d053a33ff77beee82978009eb4eceacc288
xIvan0ff <hrisivanov2@gmail.com>
2022-02-14 00:20:59 +0200

    Fixed old php stuff...

--- iplookup.php
+++ iplookup.php
@@ -4,19 +4,16 @@
 
 $ip = $_GET['host'];
 
-if($ip != NULL && $filter_var($ip, FILTER_VALIDATE_IP) !== false)
-{
+if ($ip != NULL && $filter_var($ip, FILTER_VALIDATE_IP) !== false) {
     $resp = file_get_contents("http://ip-api.com/json/$ip?fields=520191&lang=en");
-    $resp = str_replace('{', NULL, $resp);
-    $resp = str_replace('}', NULL, $resp);
-    $resp = str_replace('"', NULL, $resp);
+    $resp = str_replace('{', "", $resp);
+    $resp = str_replace('}', "", $resp);
+    $resp = str_replace('"', "", $resp);
     $resp = str_replace(':', ': ', $resp);
     $resp = str_replace(",", "\r\n", $resp);
-    $resp = str_replace("\r\nstatus: success", NULL, $resp);
+    $resp = str_replace("\r\nstatus: success", "", $resp);
     echo $resp;
     //echo "IP -> $ip";
-}
-else
-{
+} else {
     echo "[IPLookup-API] Syntax Error, Example: http://1.1.1.1/iplookup.php?host=1.3.3.7";
 }
